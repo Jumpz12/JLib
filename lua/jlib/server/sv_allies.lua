77e4d66682bbc4c3ca3eb996f975511aeb898508
Euan Medway <euanmedway@yahoo.co.uk>
2019-02-25 16:54:19 +0000

    Completed Gui for ally system and added extra checks.

--- lua/jlib/server/sv_allies.lua
+++ lua/jlib/server/sv_allies.lua
@@ -1,5 +1,11 @@
+util.AddNetworkString("_JLib_AlliesMenu")
+util.AddNetworkString("_JLib_AlliesInformation")
+
 local function Allies_Command(ply, text)
     local sides
+    local found = false
+
+
     if not ply:IsValid() then return end
     local args = string.Split(text, " ")
     local rest = table.concat(args, " ", 2)
@@ -10,10 +16,25 @@ local function Allies_Command(ply, text)
         end
     end
 
+    if JLib.Config.PlanetControl.Factions[sides][rest] == nil then
+        ply:ChatPrint("Sorry, you can't ally with this faction")
+        return
+    end
+
+    if timer.Exists(string.Replace(ply:getJobTable().category, " ", "") .. "_" .. string.Replace(rest, " ", "") .. "_JLib_AllyCoolDown") then 
+        ply:ChatPrint("Sorry, you cannot request to ally with this faction for " .. timer.TimeLeft(string.Replace(ply:getJobTable().category, " ", "") .. "_" .. string.Replace(rest, " ", "") .. "_JLib_AllyCoolDown") .. " seconds")
+        return
+    end
+
     if not table.HasValue(JLib.Config.PlanetControl.Factions[sides][ply:getJobTable().category]["Leaders"], ply:Team()) then return end 
 
     if #JLib.Config.PlanetControl.Factions[sides][ply:getJobTable().category]["Allies"] ~= 0 then
         ply:ChatPrint("Sorry you have the max amount of allies")
+        return
+    end
+
+    if #JLib.Config.PlanetControl.Factions[sides][rest]["Allies"] ~= 0 then
+        ply:ChatPrint("Sorry, they have the max amount of allies")
         return
     end
 
@@ -22,15 +43,32 @@ local function Allies_Command(ply, text)
         return
     end
 
-    if JLib.Config.PlanetControl.Factions[sides][rest] == nil then
-        ply:ChatPrint("Sorry, you can't ally with this faction")
-        return
+    for k, v in pairs(player.GetAll()) do
+        if not found then
+            if table.HasValue(JLib.Config.PlanetControl.Factions[sides][rest]["Leaders"], v:Team()) then
+                found = true
+                net.Start("_JLib_AlliesMenu")
+                net.WriteEntity(ply)
+                net.WriteTable(
+                    {side = sides, myfac = rest, fac = ply:getJobTable().category}
+                )
+                net.Send(v)
+            end
+        end
     end
-
-    table.insert(JLib.Config.PlanetControl.Factions[sides][ply:getJobTable().category]["Allies"], rest)
-    table.insert(JLib.Config.PlanetControl.Factions[sides][rest]["Allies"], ply:getJobTable().category)
-    ply:ChatPrint(rest .. " is now your ally")
-
 end
-
 hook.Add("PlayerSay", "Allies_Command", Allies_Command)
+
+net.Receive("_JLib_AlliesInformation", function()
+    local ply, info, ans = net.ReadEntity(), net.ReadTable(), net.ReadBool()
+    if ans then
+        table.insert(JLib.Config.PlanetControl.Factions[info.side][info.fac]["Allies"], info.myfac)
+        table.insert(JLib.Config.PlanetControl.Factions[info.side][info.myfac]["Allies"], info.fac)
+        ply:ChatPrint(info.myfac .. " is now your ally")
+    else
+        ply:ChatPrint(info.myfac .. " declined your ally request")
+        timer.Create(string.Replace(info.fac, " ", "") .. "_" .. string.Replace(info.myfac, " ", "") .. "_JLib_AllyCoolDown", 300, 1, function()
+            ply:ChatPrint("You can now request an alliance with" .. info.myfac)
+        end)
+    end
+end)
