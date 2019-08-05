d77236da705dfe195da58c69bdefd25ef8f88539
Euan Medway <euanmedway86@gmail.com>
2019-08-05 23:36:24 +0100

    Added player limit for neutral planet takeovers.

--- lua/jlib/shared/sh_config.lua
+++ lua/jlib/shared/sh_config.lua
@@ -352,3 +352,4 @@ hook.Add("PostGamemodeLoaded", "Table_Load", function()
 
 end )
 JLib.Config.PlanetControl.Minimum = 4 -- How many a side must have for a raid to be activated
+JLib.Config.PlanetControl.Neutral_Limit = 15 -- How many players must be on the server to take a neutral planet.
